639c26f99b6e46af5ecee03fcb75ef2a8e5cc1d9
Léo <leogaudinoff@gmail.com>
2023-03-20 19:30:06 +0100

    Try to add a cool map component in the modal view

--- application/src/components/organisms/Result.tsx
+++ application/src/components/organisms/Result.tsx
@@ -1,5 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Alert, Modal, Pressable, Text, View} from 'react-native';
+import GetLocation, {Location} from 'react-native-get-location';
+import MapView from 'react-native-maps';
 import globalStyles from '../../styles/GlobalStyles';
 
 export default function Result({
@@ -12,6 +14,27 @@ export default function Result({
   data: object;
 }) {
   const [locationLoaded, setLocationLoaded] = useState(false);
+  const [userLocation, setUserLocation] = useState<any>({});
+  const [componentMounted, setComponentMounted] = useState(false);
+
+  useEffect(() => {
+    setComponentMounted(true);
+  });
+
+  if (!locationLoaded) {
+    GetLocation.getCurrentPosition({
+      enableHighAccuracy: true,
+      timeout: 60000,
+    })
+      .then(location => {
+        setUserLocation(location);
+        setLocationLoaded(true);
+      })
+      .catch(error => {
+        const {code, message} = error;
+        console.warn(code, message);
+      });
+  }
 
   return (
     <>
@@ -29,12 +52,33 @@ export default function Result({
             <Text style={globalStyles.title}>Scan result</Text>
             <Text>
               <Text style={{fontWeight: 'bold'}}>Delivery note n°</Text>
-              <Text>{data["DN #" as keyof typeof data]}</Text>
+              <Text>{data['DN #' as keyof typeof data]}</Text>
             </Text>
             <Text>
               <Text style={{fontWeight: 'bold'}}>Scan time : </Text>
-              <Text>{Date().toLocaleString()}</Text>
+              <Text>
+                {locationLoaded
+                  ? new Date(userLocation.time).toLocaleString()
+                  : 'Determining current time...'}
+              </Text>
             </Text>
+            {locationLoaded && componentMounted ? (
+              // <MapView
+              //   // initialRegion={{
+              //   //   latitude: 36.69933699173192,
+              //   //   longitude: -4.438967710686544,
+              //   //   latitudeDelta: 0.4,
+              //   //   longitudeDelta: 0.3,
+              //   // }}
+              //   style={{flex: 1,}}
+              // />
+              <Text>
+                <Text style={{fontWeight: 'bold'}}>Latitude : </Text>
+                <Text>{userLocation.latitude}</Text>
+              </Text>
+            ) : (
+              <Text>Determining current location.../</Text>
+            )}
             <View style={globalStyles.horizontal}>
               <Pressable
                 style={globalStyles.button}
@@ -44,7 +88,9 @@ export default function Result({
               <Pressable
                 style={globalStyles.mainButton}
                 onPress={() => setModalVisible(false)}>
-                <Text style={[globalStyles.white, {fontWeight: 'bold'}]}>Send</Text>
+                <Text style={[globalStyles.white, {fontWeight: 'bold'}]}>
+                  Send
+                </Text>
               </Pressable>
             </View>
           </View>
