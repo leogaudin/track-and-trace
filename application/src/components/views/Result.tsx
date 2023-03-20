cebc469118eba1cf04bb26102bec5690812e8bd2
Léo <leogaudinoff@gmail.com>
2023-03-20 11:16:12 +0100

    Add a separate file for Result of scan just in case

---
+++ application/src/components/views/Result.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import {Alert, Pressable, Text, View} from 'react-native';
+import globalStyles from '../../styles/GlobalStyles';
+
+export default function Result({}) {}
