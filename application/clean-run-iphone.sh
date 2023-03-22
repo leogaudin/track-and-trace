85269e1dec2af667f1d1facdfd5c4f5021c550cc
Léo <leogaudinoff@gmail.com>
2023-03-22 15:55:36 +0100

    Clean run script now shuts down any running server instance

--- application/clean-run-iphone.sh
+++ application/clean-run-iphone.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
+watchman watch-del-all
+watchman shutdown-server
 xattr -w com.apple.xcode.CreatedByBuildSystem true /Users/leogaudin/Documents/Repositories/track-and-trace/application/ios/build
 cd ios
 xcodebuild clean
