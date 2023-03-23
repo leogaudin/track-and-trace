02f1fbaca42647c271de1d3e34e02c33d302ed4f
Léo <leogaudinoff@gmail.com>
2023-03-23 10:13:13 +0100

    Add a functional permissions checker

--- application/App.tsx
+++ application/App.tsx
@@ -1,51 +1,59 @@
-import React, {useState} from 'react';
-import {
-  Platform,
-  Pressable,
-  SafeAreaView,
-  Text,
-  View,
-} from 'react-native';
+import React, {useState, useEffect, useLayoutEffect} from 'react';
+import {Platform, Pressable, SafeAreaView, Text, View} from 'react-native';
 import ScanScreen from './src/components/views/QR';
 import globalStyles from './src/styles/GlobalStyles';
 import {
-  checkAndroidPermissions,
-  checkIOSPermissions,
+  handleCameraPermissionAndroid,
+  handleCameraPermissionIOS,
+  handleLocationPermissionAndroid,
+  handleLocationPermissionIOS,
 } from './src/utils/checkPermissions';
+import RNRestart from 'react-native-restart';
 
 function App(): JSX.Element {
   const [hasPermissions, setHasPermissions] = useState(false);
+  let checker = (arr: Boolean[]) => arr.every(Boolean);
 
-  if (Platform.OS === 'android') {
-    setHasPermissions(checkAndroidPermissions());
-  } else if (Platform.OS === 'ios') {
-    setHasPermissions(checkIOSPermissions());
-  }
-  return (
-    <SafeAreaView style={globalStyles.view}>
-      {hasPermissions ? (
+  useEffect(() => {
+    if (Platform.OS === 'ios') {
+      Promise.all([
+        handleCameraPermissionIOS(),
+        handleLocationPermissionIOS(),
+      ]).then(permissions => {
+        setHasPermissions(checker(permissions));
+      });
+    } else if (Platform.OS === 'android') {
+      Promise.all([
+        handleCameraPermissionAndroid(),
+        handleLocationPermissionAndroid(),
+      ]).then(permissions => {
+        setHasPermissions(checker(permissions));
+      });
+    }
+  }, []);
+
+  if (hasPermissions) {
+    return (
+      <SafeAreaView>
         <ScanScreen />
-      ) : (
-        <View style={{height: '100%'}}>
-          <Text style={[globalStyles.title, {color: 'red'}]}>
-            The application does not have all the permissions necessary to
-            launch.
-          </Text>
-          <Pressable
-            style={globalStyles.button}
-            onPress={() => {
-              if (Platform.OS === 'android') {
-                setHasPermissions(checkAndroidPermissions());
-              } else if (Platform.OS === 'ios') {
-                setHasPermissions(checkIOSPermissions());
-              }
-            }}>
-            <Text>I have updated the permissions, check again.</Text>
-          </Pressable>
-        </View>
-      )}
-    </SafeAreaView>
-  );
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <View style={{backgroundColor: 'red'}}>
+        <Text style={[globalStyles.title, {color: 'red'}]}>
+          The application does not have all the permissions necessary to launch.
+        </Text>
+        <Pressable
+          style={globalStyles.button}
+          onPress={() => {
+            RNRestart.restart();
+          }}>
+          <Text>I have updated the permissions, check again.</Text>
+        </Pressable>
+      </View>
+    );
+  }
 }
 
 export default App;
--- application/src/utils/checkPermissions.tsx
+++ application/src/utils/checkPermissions.tsx
@@ -1,132 +1,48 @@
-import {Alert, Linking} from 'react-native';
 import {check, PERMISSIONS, request, RESULTS} from 'react-native-permissions';
 
-export function checkAndroidPermissions(): boolean {
-  check(PERMISSIONS.ANDROID.CAMERA).then(result => {
-    switch (result) {
-      case RESULTS.UNAVAILABLE:
-        Alert.alert(
-          'Camera not available',
-          "Your device's camera is unavailable.",
-          [{text: 'OK', style: 'default'}],
-        );
-        return false;
-      case RESULTS.DENIED:
-        Alert.alert(
-          'Camera not enabled',
-          'Please allow access to the camera.',
-          [
-            {text: 'Cancel', style: 'cancel'},
-            {text: 'OK', style: 'default'},
-          ],
-        );
-        request(PERMISSIONS.ANDROID.CAMERA);
-        checkAndroidPermissions();
-        break;
-    }
-  });
-  check(PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION).then(result => {
-    switch (result) {
-      case RESULTS.UNAVAILABLE:
-        Alert.alert(
-          'Location not available',
-          'Your device cannot give its location.',
-          [{text: 'OK', style: 'default'}],
-        );
-        return false;
-      case RESULTS.DENIED:
-        Alert.alert(
-          'Location not enabled',
-          'Please allow access to the location.',
-          [
-            {text: 'Cancel', style: 'cancel'},
-            {text: 'OK', style: 'default'},
-          ],
-        );
-        request(PERMISSIONS.ANDROID.CAMERA);
-        checkAndroidPermissions();
-        break;
-    }
-  });
-  return true;
-}
-
-export function checkIOSPermissions(): boolean {
-  check(PERMISSIONS.IOS.CAMERA).then(result => {
-    switch (result) {
-      case RESULTS.UNAVAILABLE:
-        Alert.alert(
-          'Camera not available',
-          "Your device's camera is unavailable.",
-          [{text: 'OK', style: 'default'}],
-        );
-        return false;
-      case RESULTS.DENIED:
-        Alert.alert(
-          'Camera not enabled',
-          'Please allow access to the camera.',
-          [
-            {text: 'Cancel', style: 'cancel'},
-            {text: 'OK', style: 'default'},
-          ],
-        );
-        request(PERMISSIONS.IOS.CAMERA);
-        checkIOSPermissions();
-        break;
-      case RESULTS.BLOCKED:
-        Alert.alert(
-          'Camera not enabled',
-          'Please allow access to the camera.',
-          [
-            {text: 'Cancel', style: 'cancel'},
-            {
-              text: 'Go to Settings',
-              style: 'default',
-              onPress: () => Linking.openSettings(),
-            },
-          ],
-        );
-        checkIOSPermissions();
-        break;
-    }
-  });
-  check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE).then(result => {
-    switch (result) {
-      case RESULTS.UNAVAILABLE:
-        Alert.alert(
-          'Location not available',
-          "Your device's camera is unavailable.",
-          [{text: 'OK', style: 'default'}],
-        );
-        return false;
-      case RESULTS.DENIED:
-        Alert.alert(
-          'Location not enabled',
-          'Please allow access to the location.',
-          [
-            {text: 'Cancel', style: 'cancel'},
-            {text: 'OK', style: 'default'},
-          ],
-        );
-        request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-        checkIOSPermissions();
-        break;
-      case RESULTS.BLOCKED:
-        Alert.alert(
-          'Location blocked',
-          'Please allow access to the location in your settings.',
-          [
-            {text: 'Cancel', style: 'cancel'},
-            {
-              text: 'Go to Settings',
-              style: 'default',
-              onPress: () => Linking.openSettings(),
-            },
-          ],
-        );
-        checkIOSPermissions();
-        break;
-    }
-  });
-  return true;
-}
+export const handleCameraPermissionIOS = async () => {
+  const resCam = await check(PERMISSIONS.IOS.CAMERA);
+  if (resCam === RESULTS.GRANTED) {
+    return true;
+  } else if (resCam === RESULTS.DENIED) {
+    const resCam2 = await request(PERMISSIONS.IOS.CAMERA);
+    return resCam2 === RESULTS.GRANTED;
+  } else {
+    return false;
+  }
+};
+export const handleLocationPermissionIOS = async () => {
+  const resLoc = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+  if (resLoc === RESULTS.GRANTED) {
+    return true;
+  } else if (resLoc === RESULTS.DENIED) {
+    const resLoc2 = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    return resLoc2 === RESULTS.GRANTED;
+  } else {
+    return false;
+  }
+};
+export const handleCameraPermissionAndroid = async () => {
+  const resCam = await check(PERMISSIONS.ANDROID.CAMERA);
+  if (resCam === RESULTS.GRANTED) {
+    return true;
+  } else if (resCam === RESULTS.DENIED) {
+    const resCam2 = await request(PERMISSIONS.ANDROID.CAMERA);
+    return resCam2 === RESULTS.GRANTED;
+  } else {
+    return false;
+  }
+};
+export const handleLocationPermissionAndroid = async () => {
+  const resLoc = await check(PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION);
+  if (resLoc === RESULTS.GRANTED) {
+    return true;
+  } else if (resLoc === RESULTS.DENIED) {
+    const resLoc2 = await request(
+      PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION,
+    );
+    return resLoc2 === RESULTS.GRANTED;
+  } else {
+    return false;
+  }
+};
