7c7198bc75e034e12b24e2d679fa47cb4b7c87b8
Léo <leogaudinoff@gmail.com>
2023-03-31 18:08:02 +0200

    feat(client): :sparkles: Add methods to retrieve data from the server

---
+++ web/client/src/service/index.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export function getBoxes() {
+	return new Promise(resolve => {
+		axios.get("http://localhost:3000/api/boxes")
+			.then(res => res.status === 200 && res.data)
+			.then(res => res.data)
+			.then(resolve)
+			.catch(console.error);
+	});
+}
+
+export function getScans() {
+	return new Promise(resolve => {
+		axios.get("http://localhost:3000/api/scans")
+			.then(res => res.status === 200 && res.data)
+			.then(res => res.data)
+			.then(resolve)
+			.catch(console.error);
+	});
+}
+
+export function getAdmins() {
+	return new Promise(resolve => {
+		axios.get("http://localhost:3000/api/admins")
+			.then(res => res.status === 200 && res.data)
+			.then(res => res.data)
+			.then(resolve)
+			.catch(console.error);
+	});
+}
