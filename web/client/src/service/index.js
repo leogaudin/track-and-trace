8dedbabd4a6429ebc269f9ed7404da3e6b9de0a9
Léo <leogaudinoff@gmail.com>
2023-04-02 18:48:28 +0200

    feat(api): :sparkles: Add a method to retrieve info about one box

--- web/client/src/service/index.js
+++ web/client/src/service/index.js
@@ -10,6 +10,16 @@ export function getBoxes() {
 	});
 }
 
+export function getBox(id) {
+	return new Promise(resolve => {
+		axios.get("http://localhost:3000/api/box/" + id)
+			.then(res => res.status === 200 && res.data)
+			.then(res => res.data)
+			.then(resolve)
+			.catch(console.error);
+	});
+}
+
 export function getScans() {
 	return new Promise(resolve => {
 		axios.get("http://localhost:3000/api/scans")
