d2020bdffc1d7585c8887d16eabdd5b826cd4908
Léo <leogaudinoff@gmail.com>
2023-04-03 19:47:17 +0200

    style(client): :lipstick: Change title component

--- web/client/src/components/BoxSummary.js
+++ web/client/src/components/BoxSummary.js
@@ -1,4 +1,4 @@
-import { Dialog, DialogContent, DialogTitle, Card, CardHeader, Stack, Item, Typography } from "@mui/material";
+import { Dialog, DialogContent, Card, Stack, Typography, CardContent } from "@mui/material";
 import QRCode from "react-qr-code";
 import { getBox } from "../service";
 import { useEffect, useState } from "react";
@@ -21,28 +21,35 @@ export default function BoxSummary({ id, open, setOpen }) {
 			onClose={handleClose}
 			scroll='body'
 			keepMounted={false}>
-			<DialogTitle>Box summary</DialogTitle>
 			<DialogContent>
+				<Typography
+					variant='overline'
+				>Box summary</Typography>
 				<Card>
-					<CardHeader title='Informations' />
-					<Stack direction={'row'} spacing={5} padding={3} alignItems={'flex-start'}>
-						<Stack direction={'column'} spacing={.5}>
-							<Typography>Recipient: <b>{boxData?.school}</b></Typography>
-							<Typography>Division: <b>{boxData?.division}</b></Typography>
-							<Typography>District: <b>{boxData?.district}</b></Typography>
-							<Typography>Zone: <b>{boxData?.zone}</b></Typography>
-							<Typography>Institution type: <b>{boxData?.institutionType}</b></Typography>
-							<Typography>Person in charge: <b>{boxData?.htName}</b></Typography>
-							<Typography>Phone: <b>{boxData?.htPhone}</b></Typography>
-							<Typography>Created: <b>{
-								new Date(boxData?.createdAt).toLocaleString()
-							}</b></Typography>
+					<CardContent>
+						<Typography
+							color="text.secondary"
+							variant="overline"
+						>Informations</Typography>
+						<Stack direction={'row'} spacing={5} alignItems={'flex-start'}>
+							<Stack direction={'column'} spacing={.5}>
+								<Typography>Recipient: <b>{boxData?.school}</b></Typography>
+								<Typography>Division: <b>{boxData?.division}</b></Typography>
+								<Typography>District: <b>{boxData?.district}</b></Typography>
+								<Typography>Zone: <b>{boxData?.zone}</b></Typography>
+								<Typography>Institution type: <b>{boxData?.institutionType}</b></Typography>
+								<Typography>Person in charge: <b>{boxData?.htName}</b></Typography>
+								<Typography>Phone: <b>{boxData?.htPhone}</b></Typography>
+								<Typography>Created: <b>{
+									new Date(boxData?.createdAt).toLocaleString()
+								}</b></Typography>
+							</Stack>
+							<Stack direction={'column'} spacing={1} alignItems={'center'}>
+								<QRCode value={id} size={100} />
+								<Typography fontFamily={'Consolas, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New'} fontSize={'.7rem'}>{id}</Typography>
+							</Stack>
 						</Stack>
-						<Stack direction={'column'} spacing={1} alignItems={'center'}>
-							<QRCode value={id} size={100} />
-							<Typography fontFamily={'Consolas, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New'} fontSize={'.7rem'}>{id}</Typography>
-						</Stack>
-					</Stack>
+					</CardContent>
 				</Card>
 				{/* <Card>
 					<CardHeader title='Location summary' />
