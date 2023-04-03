3b25d6ed579fea00cc6cffb8c6b1a93fb4b25ce7
Léo <leogaudinoff@gmail.com>
2023-04-03 19:47:58 +0200

    style(client): :lipstick: Change title component

--- web/client/src/components/BoxesOverview.js
+++ web/client/src/components/BoxesOverview.js
@@ -1,14 +1,11 @@
-import { CardHeader, Table, Card, TableHead, TableCell, TableRow, TableBody, Typography } from '@mui/material';
+import { Table, Card, TableHead, TableCell, TableRow, TableBody, Typography, CardContent, Box } from '@mui/material';
 import { getBoxes, getScans } from '../service';
-import { createTheme } from '../theme/index';
-import { ThemeProvider } from '@emotion/react';
 import {
 	useState, useEffect
 } from 'react';
 import BoxSummary from './BoxSummary';
 import { SeverityPill } from './SeverityPill';
 
-const theme = createTheme();
 const statusMap = {
 	inprogress: 'warning',
 	delivered: 'success',
@@ -29,54 +26,64 @@ export default function BoxesOverview() {
 	}, []);
 
 	return (
-		<Card>
-			<CardHeader title='Your tracked items' />
-			<Table>
-				<TableHead>
-					<TableRow>
-						<TableCell>
-							ID
-						</TableCell>
-						<TableCell>
-							Recipient
-						</TableCell>
-						<TableCell sortDirection="desc">
-							Created
-						</TableCell>
-						<TableCell>
-							Status
-						</TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{boxes.map((box) => {
-						const date = new Date(box?.createdAt);
-						return (
-							<TableRow id={box?.id} onClick={() => {
-								setBoxID(box?.id);
-								setBoxDialogOpen(true);
-							}}
-								hover={true}>
-								<TableCell>
-									<Typography fontFamily={'Consolas, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New'} fontSize={'.8rem'}>{box?.id}</Typography>
-								</TableCell>
-								<TableCell>
-									{box?.school}
-								</TableCell>
-								<TableCell>
-									{date.getHours() + ":" + date.getMinutes() + ", " + date.toLocaleDateString()}
-								</TableCell>
-								<TableCell>
-									<SeverityPill color={statusMap['inprogress']}>
-										{'In Progress'}
-									</SeverityPill>
-								</TableCell>
-							</TableRow>
-						)
-					})}
-				</TableBody>
-			</Table>
-			<BoxSummary id={boxID} open={boxDialogOpen} setOpen={setBoxDialogOpen} />
+		<Card
+			style={{
+				width: '100%',
+				height: '100%',
+				maxHeight: '25rem',
+				overflow: 'auto'
+			}}>
+			<CardContent>
+				<Typography
+					variant='overline'
+				>Your tracked items</Typography>
+				<Table>
+					<TableHead>
+						<TableRow>
+							<TableCell>
+								ID
+							</TableCell>
+							<TableCell>
+								Recipient
+							</TableCell>
+							<TableCell sortDirection="desc">
+								Created
+							</TableCell>
+							<TableCell>
+								Status
+							</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{boxes.map((box) => {
+							const date = new Date(box?.createdAt);
+							return (
+								<TableRow id={box?.id} onClick={() => {
+									setBoxID(box?.id);
+									setBoxDialogOpen(true);
+								}}
+									hover={true}>
+									<TableCell>
+										<Typography fontFamily={'Consolas, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New'} fontSize={'.8rem'}>{box?.id}</Typography>
+									</TableCell>
+									<TableCell>
+										{box?.school}
+									</TableCell>
+									<TableCell>
+										{date.getHours() + ":" + date.getMinutes() + ", " + date.toLocaleDateString()}
+									</TableCell>
+									<TableCell>
+										<SeverityPill color={statusMap['inprogress']}>
+											{'In Progress'}
+										</SeverityPill>
+									</TableCell>
+								</TableRow>
+							)
+						})}
+					</TableBody>
+				</Table>
+				<BoxSummary id={boxID} open={boxDialogOpen} setOpen={setBoxDialogOpen} />
+			</CardContent>
 		</Card>
 	);
 }
