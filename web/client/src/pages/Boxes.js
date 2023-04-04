838809aa9aa437c39e96a8471f5eadd41b9127c0
Léo <leogaudinoff@gmail.com>
2023-04-04 19:20:51 +0200

    perf(web): :zap: Change component's way of calling API
    Every BoxesOverview component would call getBoxes once, and every BoxSummary would call getBox once too. Now getBoxes is called one time and passed through components.

--- web/client/src/pages/Boxes.js
+++ web/client/src/pages/Boxes.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import BoxesOverview from '../components/BoxesOverview';
 import { Box } from '@mui/material';
+import { getBoxes } from '../service';
 
 export default function Boxes() {
+	const [boxes, setBoxes] = useState([]);
+
+	useEffect(() => {
+		getBoxes()
+			.then(setBoxes)
+	}, [])
 	return (
 		<Box
 			padding={3}
 			display={'flex'}
 			width={'100%'}
 		>
-			<BoxesOverview />
+			<BoxesOverview boxes={boxes} />
 		</Box>
 	);
 }
