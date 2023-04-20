d22cc22e02e380fe2e74e35cfc7699d03e4b2dc8
Léo <leogaudinoff@gmail.com>
2023-04-20 18:04:54 +0200

    feat(client): :lipstick: Add a circular progress with label component

---
+++ web/client/src/components/CircularProgressWithLabel.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+export default function CircularProgressWithLabel(props) {
+  return (
+    <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+      <CircularProgress {...props} size={70} />
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: 'absolute',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Typography variant="caption" component="div" color="success.main">
+          {`${Math.round(props.value)}%`}
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
