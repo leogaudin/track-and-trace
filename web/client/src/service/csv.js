9b6e792419cbb9070e6d3ee553000a3d46c29dda
Léo <leogaudinoff@gmail.com>
2023-04-23 19:55:11 +0200

    chore(client): :coffin: Remove dead code

--- web/client/src/service/csv.js
+++ web/client/src/service/csv.js
@@ -8,59 +8,6 @@ function isCSVValid(file) {
 	return 1;
 }
 
-// function parseCSV(text, lines, setUploadProgress, results, setResults) {
-// 	// set buffer size
-// 	const BUFFER_SIZE = 5;
-// 	let boxes = [];
-// 	let uploaded = 0;
-// 	// parse the csv file
-// 	Papa.parse(text, {
-// 		worker: true,
-// 		step: (element) => {
-// 			// create a box object
-// 			const box = {
-// 				id: element.data[0],
-// 				division: element.data[1],
-// 				district: element.data[2],
-// 				zone: element.data[3],
-// 				school: element.data[4],
-// 				htName: element.data[5],
-// 				htPhone: element.data[6],
-// 				institutionType: element.data[7],
-// 				adminId: 'lecuistot'
-// 			};
-// 			// add it to the buffer
-// 			boxes.push(box);
-// 			// if the buffer is full, upload it
-// 			if (boxes.length >= BUFFER_SIZE) {
-// 				let temp = boxes;
-// 				boxes = [];
-// 				// upload the buffer
-// 				addBoxes(temp)
-// 					.then((success) => {
-// 						// update the progress bar
-// 						uploaded += temp.length;
-// 						setUploadProgress(uploaded / lines);
-// 						setResults(results => [...results, success]);
-// 					})
-// 			}
-// 		},
-// 		complete: (results) => {
-// 			// upload the last buffer
-// 			let temp = boxes;
-// 			boxes = [];
-// 			addBoxes(temp)
-// 				.then((success) => {
-// 					uploaded += temp.length;
-// 					console.log(uploaded);
-// 					console.log(lines);
-// 					setUploadProgress(uploaded / lines);
-// 					setResults(results => [...results, success]);
-// 				})
-// 		}
-// 	})
-// }
-
 function parseCSV(text, setUploadProgress, results, setResults) {
 	let boxes = [];
 	Papa.parse(text, {
@@ -87,7 +34,7 @@ function parseCSV(text, setUploadProgress, results, setResults) {
 
 function uploadBoxes(boxes, setUploadProgress, setResults) {
 	const BOXES_LENGTH = boxes.length;
-	const BUFFER_SIZE = 5;
+	const BUFFER_SIZE = 21;
 	let uploaded = 0;
 	for (let i = 0; i < boxes.length; i += BUFFER_SIZE) {
 		const chunk = boxes.slice(i, i + BUFFER_SIZE);
