24ad8bdde482b81f391da32919c764b0ec375b72
Léo <leogaudinoff@gmail.com>
2023-04-23 19:00:07 +0200

    feat(api): :sparkles: Implement successful parser and uploader for CSVs

--- web/client/src/components/DragAndDrop.js
+++ web/client/src/components/DragAndDrop.js
@@ -1,36 +1,41 @@
 import React, { useState } from 'react';
 import { handleCSV } from '../service/csv';
+import CircularProgressWithLabel from '../components/CircularProgressWithLabel';
 
 export default function DragAndDrop() {
 	const [hover, setHover] = useState(false);
-	const [counter, setCounter] = useState(0);
-	const [lines, setLines] = useState(0)
-
-	function incrementCounter() {
-		setCounter(counter => counter + 1);
-	}
+	const [uploadProgress, setUploadProgress] = useState(0);
+	const [isLoading, setIsLoading] = useState(false);
+	const [results, setResults] = useState([]);
 
 	const handleDragEnter = e => {
 		e.preventDefault();
 		setHover(true);
 		e.stopPropagation();
 	};
+
 	const handleDragLeave = e => {
 		e.preventDefault();
 		setHover(false);
 		e.stopPropagation();
 	};
+
 	const handleDragOver = e => {
 		e.preventDefault();
 		setHover(true);
 		e.stopPropagation();
 	};
+
 	const handleDrop = e => {
 		e.preventDefault();
-		handleCSV(e.dataTransfer.files, incrementCounter, setLines);
+		setIsLoading(true);
+		handleCSV(e.dataTransfer.files, setUploadProgress, results, setResults);
 		setHover(false);
 		e.stopPropagation();
 	};
+
+	console.log(results);
+
 	return (
 		<div
 			className={'drag-drop-zone'}
@@ -48,8 +53,14 @@ export default function DragAndDrop() {
 			onDragEnter={e => handleDragEnter(e)}
 			onDragLeave={e => handleDragLeave(e)}
 		>
-			<p>Drag files here to upload</p>
-			<p>{counter}/{lines} uploaded</p>
+			{isLoading ?
+				(
+					<CircularProgressWithLabel color='success' value={uploadProgress * 100} />
+				) :
+				(
+					<p>Drag files here to upload</p>
+				)
+			}
 		</div>
 	);
 };
--- web/client/src/service/csv.js
+++ web/client/src/service/csv.js
@@ -1,13 +1,68 @@
 import Papa from 'papaparse';
-import { addBox } from '.';
+import { addBoxes } from './index';
 
 function isCSVValid(file) {
+	// check if the file is a csv
 	if (file.type !== 'text/csv')
 		return 'File is not CSV';
-	return 'OK';
+	return 1;
 }
 
-function parseCSV(text, incrementCounter) {
+// function parseCSV(text, lines, setUploadProgress, results, setResults) {
+// 	// set buffer size
+// 	const BUFFER_SIZE = 5;
+// 	let boxes = [];
+// 	let uploaded = 0;
+// 	// parse the csv file
+// 	Papa.parse(text, {
+// 		worker: true,
+// 		step: (element) => {
+// 			// create a box object
+// 			const box = {
+// 				id: element.data[0],
+// 				division: element.data[1],
+// 				district: element.data[2],
+// 				zone: element.data[3],
+// 				school: element.data[4],
+// 				htName: element.data[5],
+// 				htPhone: element.data[6],
+// 				institutionType: element.data[7],
+// 				adminId: 'lecuistot'
+// 			};
+// 			// add it to the buffer
+// 			boxes.push(box);
+// 			// if the buffer is full, upload it
+// 			if (boxes.length >= BUFFER_SIZE) {
+// 				let temp = boxes;
+// 				boxes = [];
+// 				// upload the buffer
+// 				addBoxes(temp)
+// 					.then((success) => {
+// 						// update the progress bar
+// 						uploaded += temp.length;
+// 						setUploadProgress(uploaded / lines);
+// 						setResults(results => [...results, success]);
+// 					})
+// 			}
+// 		},
+// 		complete: (results) => {
+// 			// upload the last buffer
+// 			let temp = boxes;
+// 			boxes = [];
+// 			addBoxes(temp)
+// 				.then((success) => {
+// 					uploaded += temp.length;
+// 					console.log(uploaded);
+// 					console.log(lines);
+// 					setUploadProgress(uploaded / lines);
+// 					setResults(results => [...results, success]);
+// 				})
+// 		}
+// 	})
+// }
+
+function parseCSV(text, setUploadProgress, results, setResults) {
+	let boxes = [];
 	Papa.parse(text, {
 		worker: true,
 		step: (element) => {
@@ -22,25 +77,65 @@ function parseCSV(text, incrementCounter) {
 				institutionType: element.data[7],
 				adminId: 'lecuistot'
 			};
-			addBox(box)
-				.then((res) => {
-					console.log(res);
-					incrementCounter();
-				});
+			boxes.push(box);
 		},
-		complete: (results) => {
-			console.log('Uploaded all the boxes')
+		complete: () => {
+			uploadBoxes(boxes, setUploadProgress, setResults);
 		}
 	})
 }
 
-export function handleCSV(files, incrementCounter, setLines) {
+function uploadBoxes(boxes, setUploadProgress, setResults) {
+	const BOXES_LENGTH = boxes.length;
+	const BUFFER_SIZE = 5;
+	let uploaded = 0;
+	for (let i = 0; i < boxes.length; i += BUFFER_SIZE) {
+		const chunk = boxes.slice(i, i + BUFFER_SIZE);
+		addBoxes(chunk)
+			.then((success) => {
+				uploaded += chunk.length;
+				setUploadProgress(uploaded / BOXES_LENGTH);
+				setResults(results => [...results, success]);
+				if (uploaded === BOXES_LENGTH)
+					setResults((results) => {
+						return (createSummary(results));
+					});
+			})
+	}
+}
+
+function createSummary(results) {
+	let output = {
+		invalidBoxes: [],
+		validBoxesToCreate: []
+	};
+	results.forEach((result) => {
+		result.invalidBoxes.forEach((box) => {
+			output.invalidBoxes.push(box);
+		})
+		result.validBoxesToCreate.forEach((box) => {
+			output.validBoxesToCreate.push(box);
+		})
+	})
+	return (output);
+}
+
+export function handleCSV(files, setUploadProgress, results, setResults) {
 	const file = files[0];
-	if (isCSVValid(file) !== 'OK')
+	// check if the file is a csv
+	if (isCSVValid(file) !== 1)
 		throw Error(isCSVValid(file));
+	// parse the csv file
 	file.text()
 		.then((data) => {
-			setLines(data.split("\n").length);
-			parseCSV(data, incrementCounter);
+			parseCSV(
+				data,
+				setUploadProgress,
+				results,
+				setResults
+			);
+		})
+		.catch((error) => {
+			console.log(error);
 		})
 }
