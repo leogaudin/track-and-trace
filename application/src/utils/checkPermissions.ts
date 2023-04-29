c010953ed8fa314b174afa163e2969c31dd7d9eb
Léo <leogaudinoff@gmail.com>
2023-04-29 21:40:24 +0200

    refactor(client): :zap: Refactor all the back-end to reduce technical debt

--- application/src/utils/checkPermissions.ts
+++ application/src/utils/checkPermissions.ts
@@ -1,72 +1,42 @@
-import {check, PERMISSIONS, request, RESULTS} from 'react-native-permissions';
+import {
+  check,
+  Permission,
+  PERMISSIONS,
+  request,
+  RESULTS,
+} from 'react-native-permissions';
 
 /**
- * Checks the access to the camera.
- * Immediately returns true if so.
- * Otherwise requests the permission, then checks again.
- * @returns true if the app has access to the camera, false otherwise.
- */
+Checks the access to the given permission on the given platform.
+Immediately returns true if so.
+Otherwise requests the permission, then checks again.
+@param permission The permission to check (e.g., PERMISSIONS.IOS.CAMERA or PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION).
+@returns true if the app has access to the permission, false otherwise.
+*/
+export const handlePermission = async (permission: Permission) => {
+  const res = await check(permission);
+  if (res === RESULTS.GRANTED) {
+    return true;
+  } else if (res === RESULTS.DENIED) {
+    const res_retry = await request(permission);
+    return res_retry === RESULTS.GRANTED;
+  } else {
+    return false;
+  }
+};
+
 export const handleCameraPermissionIOS = async () => {
-  const resCam = await check(PERMISSIONS.IOS.CAMERA);
-  if (resCam === RESULTS.GRANTED) {
-    return true;
-  } else if (resCam === RESULTS.DENIED) {
-    const resCam2 = await request(PERMISSIONS.IOS.CAMERA);
-    return resCam2 === RESULTS.GRANTED;
-  } else {
-    return false;
-  }
+  return await handlePermission(PERMISSIONS.IOS.CAMERA);
 };
-/**
- * Checks the access to the location.
- * Immediately returns true if so.
- * Otherwise requests the permission, then checks again.
- * @returns true if the app has access to the location, false otherwise.
- */
+
 export const handleLocationPermissionIOS = async () => {
-  const resLoc = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-  if (resLoc === RESULTS.GRANTED) {
-    return true;
-  } else if (resLoc === RESULTS.DENIED) {
-    const resLoc2 = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-    return resLoc2 === RESULTS.GRANTED;
-  } else {
-    return false;
-  }
+  return await handlePermission(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
 };
-/**
- * Checks the access to the camera.
- * Immediately returns true if so.
- * Otherwise requests the permission, then checks again.
- * @returns true if the app has access to the camera, false otherwise.
- */
+
 export const handleCameraPermissionAndroid = async () => {
-  const resCam = await check(PERMISSIONS.ANDROID.CAMERA);
-  if (resCam === RESULTS.GRANTED) {
-    return true;
-  } else if (resCam === RESULTS.DENIED) {
-    const resCam2 = await request(PERMISSIONS.ANDROID.CAMERA);
-    return resCam2 === RESULTS.GRANTED;
-  } else {
-    return false;
-  }
+  return await handlePermission(PERMISSIONS.ANDROID.CAMERA);
 };
-/**
- * Checks the access to the location.
- * Immediately returns true if so.
- * Otherwise requests the permission, then checks again.
- * @returns true if the app has access to the location, false otherwise.
- */
+
 export const handleLocationPermissionAndroid = async () => {
-  const resLoc = await check(PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION);
-  if (resLoc === RESULTS.GRANTED) {
-    return true;
-  } else if (resLoc === RESULTS.DENIED) {
-    const resLoc2 = await request(
-      PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION,
-    );
-    return resLoc2 === RESULTS.GRANTED;
-  } else {
-    return false;
-  }
+  return await handlePermission(PERMISSIONS.ANDROID.ACCESS_COARSE_LOCATION);
 };
