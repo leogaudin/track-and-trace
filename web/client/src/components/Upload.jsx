210092f12a819b01a9f3b76e4e97bf44cb0ec363
Léo <leogaudinoff@gmail.com>
2023-04-30 12:54:43 +0200

    refactor(app): :zap: Refactor to reduce technical debt and cognitive complexity

--- web/client/src/components/Upload.jsx
+++ web/client/src/components/Upload.jsx
@@ -5,108 +5,117 @@ import UploadSummary from '../components/UploadSummary';
 import { Dialog, DialogContent, Typography } from "@mui/material";
 
 export default function Upload({ open, setOpen }) {
-	const [hover, setHover] = useState(false);
-	const [uploadProgress, setUploadProgress] = useState(0);
-	const [isLoading, setIsLoading] = useState(false);
-	const [results, setResults] = useState([]);
-	const [isComplete, setComplete] = useState(false);
-	const inputFile = useRef(null);
+  const [hover, setHover] = useState(false);
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [results, setResults] = useState([]);
+  const [isComplete, setComplete] = useState(false);
+  const inputFile = useRef(null);
 
-	const handleDragEnter = e => {
-		e.preventDefault();
-		setHover(true);
-		e.stopPropagation();
-	};
+  const handleDragEnter = e => {
+    e.preventDefault();
+    setHover(true);
+    e.stopPropagation();
+  };
 
-	const handleDragLeave = e => {
-		e.preventDefault();
-		setHover(false);
-		e.stopPropagation();
-	};
+  const handleDragLeave = e => {
+    e.preventDefault();
+    setHover(false);
+    e.stopPropagation();
+  };
 
-	const handleDragOver = e => {
-		e.preventDefault();
-		setHover(true);
-		e.stopPropagation();
-	};
+  const handleDragOver = e => {
+    e.preventDefault();
+    setHover(true);
+    e.stopPropagation();
+  };
 
-	const handleDrop = e => {
-		e.preventDefault();
-		onChangeFile(e);
-		e.stopPropagation();
-	};
+  const handleDrop = e => {
+    e.preventDefault();
+    onChangeFile(e);
+    e.stopPropagation();
+  };
 
-	const handleClick = e => {
-		if (!isLoading)
-			inputFile.current.click();
-		e.stopPropagation();
-	}
+  const handleClick = e => {
+    if (!isLoading)
+      inputFile.current.click();
+    e.stopPropagation();
+  }
 
-	const onChangeFile = e => {
-		let files = [];
-		e.preventDefault();
-		setIsLoading(true);
-		setHover(false);
-		if (e.dataTransfer && e.dataTransfer.files)
-			files = e.dataTransfer.files;
-		else if (e.target.files)
-			files = e.target.files;
-		handleCSV(files, setUploadProgress, setResults, setIsLoading, setComplete);
-	}
+  const onChangeFile = e => {
+    let files = [];
+    e.preventDefault();
+    setIsLoading(true);
+    setHover(false);
+    if (e.dataTransfer && e.dataTransfer.files)
+      files = e.dataTransfer.files;
+    else if (e.target.files)
+      files = e.target.files;
+    handleCSV(files, setUploadProgress, setResults, setIsLoading, setComplete);
+  }
 
-	function handleClose() {
-		if (!isLoading && !isComplete)
-			setOpen(false);
-	}
+  function handleClose() {
+    if (!isLoading && !isComplete)
+      setOpen(false);
+  }
 
-	return (
-		<Dialog
-			open={open}
-			onClose={handleClose}
-			scroll='paper'
-			keepMounted={false}
-			maxWidth
-		>
-			<DialogContent>
-				{!isComplete ?
-					<div
-						className={isLoading ? null : 'drag-drop-zone'}
-						style={{
-							minWidth: '25rem',
-							minHeight: '25rem',
-							backgroundColor: hover ? '#10B981' : '#fff',
-							borderRadius: '1rem',
-							display: 'flex',
-							justifyContent: 'center',
-							alignItems: 'center',
-							flexDirection: 'column',
-						}}
-						onDrop={e => handleDrop(e)}
-						onDragOver={e => handleDragOver(e)}
-						onDragEnter={e => handleDragEnter(e)}
-						onDragLeave={e => handleDragLeave(e)}
-						onClick={e => handleClick(e)}
-					>
-						<input
-							type='file'
-							id='file'
-							ref={inputFile}
-							onChange={onChangeFile}
-							style={{ display: 'none' }}
-							accept='.csv'
-						/>
-						{isLoading
-							? <CircularProgressWithLabel
-								color='success'
-								value={uploadProgress * 100}
-							/>
-							: <Typography
-								variant='overline'>
-								{hover ? "Drop!" : "Click or Drag files here to upload"}
-							</Typography>}
-					</div>
-					: <UploadSummary results={results} open={open} setOpen={setOpen} />}
-			</DialogContent>
-		</Dialog>
-	);
+  const dragDropZoneStyles = {
+    minWidth: '25rem',
+    minHeight: '25rem',
+    backgroundColor: hover ? '#10B981' : '#fff',
+    borderRadius: '1rem',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+  };
+
+  const uploadFileInputStyles = {
+    display: 'none',
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      scroll='paper'
+      keepMounted={false}
+      maxWidth
+    >
+      <DialogContent>
+        {!isComplete ? (
+          <div
+            className={isLoading ? null : 'drag-drop-zone'}
+            style={dragDropZoneStyles}
+            onDrop={handleDrop}
+            onDragOver={handleDragOver}
+            onDragEnter={handleDragEnter}
+            onDragLeave={handleDragLeave}
+            onClick={handleClick}
+          >
+            <input
+              type='file'
+              id='file'
+              ref={inputFile}
+              onChange={onChangeFile}
+              style={uploadFileInputStyles}
+              accept='.csv'
+            />
+            {isLoading ? (
+              <CircularProgressWithLabel
+                color='success'
+                value={uploadProgress * 100}
+              />
+            ) : (
+              <Typography variant='overline'>
+                {hover ? "Drop!" : "Click or Drag files here to upload"}
+              </Typography>
+            )}
+          </div>
+        ) : (
+          <UploadSummary results={results} open={open} setOpen={setOpen} />
+        )}
+      </DialogContent>
+    </Dialog>
+  );
 }
