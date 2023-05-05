ff42bb920b52d3931da7d2445eb1858cb849342a
Léo <leogaudinoff@gmail.com>
2023-05-05 21:45:09 +0200

    chore(api): :truck: Change API responses from "ModelName" to "item" for consistency

--- web/server/controllers/controller.js
+++ web/server/controllers/controller.js
@@ -1,42 +1,50 @@
-const handle404Error = (Model, res) => {
-    return res.status(404).json({ success: false, error: `${Model.modelName} not found` });
+const handle404Error = (res) => {
+    return res.status(404).json({ success: false, error: `Item not found` });
 };
 
 const createOne = (Model) => async (req, res) => {
     try {
         const body = req.body;
         body.createdAt = new Date().getTime();
+
+        let status, json;
         if (!body) {
-            return res.status(400).json({
+            status = 400;
+            json = {
                 success: false,
-                error: `You must provide a ${Model.modelName}`,
-            });
+                error: `You must provide an item`,
+            };
+        } else {
+            const existent = await Model.findOne({ id: body.id });
+            if (existent) {
+                status = 409;
+                json = {
+                    success: false,
+                    error: `An item with this ID already exists: item n°${existent.id}`,
+                };
+            } else {
+                const instance = new Model(body);
+                if (!instance) {
+                    status = 400;
+                    json = { success: false, error: err };
+                } else {
+                    await instance.save();
+                    status = 201;
+                    json = {
+                        success: true,
+                        id: instance.id,
+                        message: `Item created!`,
+                    };
+                }
+            }
         }
-
-        const existent = await Model.findOne({ id: body.id });
-        if (existent) {
-            return res.status(409).json({
-                success: false,
-                error: `A ${Model.modelName} with this ID already exists: ${Model.modelName} n°${existent.id}`,
-            });
-        }
-
-        const instance = new Model(body);
-        if (!instance) {
-            return res.status(400).json({ success: false, error: err });
-        }
-
-        await instance.save();
-        return res.status(201).json({
-            success: true,
-            id: instance.id,
-            message: `${Model.modelName} created!`,
-        });
+        return res.status(status).json(json);
     } catch (error) {
         console.log(error);
         return res.status(400).json({ success: false, error: error });
     }
 };
+
 
 const createMany = (Model) => async (req, res) => {
     try {
@@ -48,7 +56,7 @@ const createMany = (Model) => async (req, res) => {
             if (!instance.id) {
                 invalidInstances.push({
                     instance,
-                    error: `You must provide an id for the ${Model.modelName}`,
+                    error: `You must provide an id for the item`,
                 });
                 continue;
             }
@@ -57,7 +65,7 @@ const createMany = (Model) => async (req, res) => {
             if (existent) {
                 invalidInstances.push({
                     instance,
-                    error: `A ${Model.modelName} with this ID already exists: ${Model.modelName} n°${existent.id}`,
+                    error: `An item with this ID already exists: item n°${existent.id}`,
                 });
                 continue;
             }
@@ -70,14 +78,14 @@ const createMany = (Model) => async (req, res) => {
         if (invalidInstances.length > 0) {
             return res.status(206).json({
                 success: true,
-                message: `Some ${Model.modelName}s were not created`,
+                message: `Some items were not created`,
                 invalidInstances,
                 validInstances,
             });
         } else {
             return res.status(201).json({
                 success: true,
-                message: `${Model.modelName}s created!`,
+                message: `Items created!`,
                 validInstances,
             });
         }
@@ -91,7 +99,7 @@ const getById = (Model) => async (req, res) => {
     try {
         const instance = await Model.findOne({ id: req.params.id });
         if (!instance)
-            return handle404Error(Model, res);
+            return handle404Error(res);
         return res.status(200).json({ success: true, data: instance });
     } catch (error) {
         console.log(error);
@@ -103,7 +111,7 @@ const getAll = (Model) => async (req, res) => {
     try {
         const instances = await Model.find({});
         if (!instances.length)
-            return handle404Error(Model, res);
+            return handle404Error(res);
         return res.status(200).json({ success: true, data: instances });
     } catch (error) {
         console.log(error);
@@ -117,7 +125,7 @@ const deleteOne = (Model) => async (req, res) => {
     try {
         const instance = await Model.findOneAndDelete({ id: req.params.id });
         if (!instance)
-            return handle404Error(Model, res);
+            return handle404Error(res);
         return res.status(200).json({ success: true, data: instance });
     } catch (error) {
         console.log(error);
