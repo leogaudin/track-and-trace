fb975487779e12f56dde30726c729d09fd1abbac
Léo <leogaudinoff@gmail.com>
2023-05-05 21:48:22 +0200

    chore(client): :bulb: Remove useless comments

--- web/client/src/service/timeAgo.js
+++ web/client/src/service/timeAgo.js
@@ -13,21 +13,19 @@ export function timeAgo(time) {
 			time = +new Date();
 	}
 	var time_formats = [
-		[60, 'seconds', 1], // 60
-		[120, '1 minute ago', '1 minute from now'], // 60*2
-		[3600, 'minutes', 60], // 60*60, 60
-		[7200, '1 hour ago', '1 hour from now'], // 60*60*2
-		[86400, 'hours', 3600], // 60*60*24, 60*60
-		[172800, 'Yesterday', 'Tomorrow'], // 60*60*24*2
-		[604800, 'days', 86400], // 60*60*24*7, 60*60*24
-		[1209600, 'Last week', 'Next week'], // 60*60*24*7*4*2
-		[2419200, 'weeks', 604800], // 60*60*24*7*4, 60*60*24*7
-		[4838400, 'Last month', 'Next month'], // 60*60*24*7*4*2
-		[29030400, 'months', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
-		[58060800, 'Last year', 'Next year'], // 60*60*24*7*4*12*2
-		[2903040000, 'years', 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
-		[5806080000, 'Last century', 'Next century'], // 60*60*24*7*4*12*100*2
-		[58060800000, 'centuries', 2903040000] // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
+		[60, 'seconds', 1],
+		[120, '1 minute ago', '1 minute from now'],
+		[3600, 'minutes', 60],
+		[7200, '1 hour ago', '1 hour from now'],
+		[86400, 'hours', 3600],
+		[172800, 'Yesterday', 'Tomorrow'],
+		[604800, 'days', 86400],
+		[1209600, 'Last week', 'Next week'],
+		[2419200, 'weeks', 604800],
+		[4838400, 'Last month', 'Next month'],
+		[29030400, 'months', 2419200],
+		[58060800, 'Last year', 'Next year'],
+		[2903040000, 'years', 29030400]
 	];
 	var seconds = (+new Date() - time) / 1000,
 		token = 'ago',
