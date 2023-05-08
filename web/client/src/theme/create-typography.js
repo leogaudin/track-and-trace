c07f6f0de3cfd6342f79f7b8a75f58ffc78bc9af
Léo <leogaudinoff@gmail.com>
2023-05-08 18:09:06 +0200

    style(client): :lipstick: Add font

--- web/client/src/theme/create-typography.js
+++ web/client/src/theme/create-typography.js
@@ -1,5 +1,5 @@
 export const createTypography = () => ({
-  fontFamily: '"Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
+  fontFamily: '"Mona Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
   body1: { fontSize: '1rem', fontWeight: 400, lineHeight: 1.5 },
   body2: { fontSize: '0.875rem', fontWeight: 400, lineHeight: 1.57 },
   button: { fontWeight: 600 },
@@ -7,10 +7,10 @@ export const createTypography = () => ({
   subtitle1: { fontSize: '1rem', fontWeight: 500, lineHeight: 1.57 },
   subtitle2: { fontSize: '0.875rem', fontWeight: 500, lineHeight: 1.57 },
   overline: { fontSize: '0.75rem', fontWeight: 600, letterSpacing: '0.5px', lineHeight: 2.5, textTransform: 'uppercase' },
-  h1: { fontFamily: '\'Plus Jakarta Sans\', sans-serif', fontWeight: 700, fontSize: '3.5rem', lineHeight: 1.2 },
-  h2: { fontFamily: '\'Plus Jakarta Sans\', sans-serif', fontWeight: 700, fontSize: '3rem', lineHeight: 1.2 },
-  h3: { fontFamily: '\'Plus Jakarta Sans\', sans-serif', fontWeight: 700, fontSize: '2.25rem', lineHeight: 1.2 },
-  h4: { fontFamily: '\'Plus Jakarta Sans\', sans-serif', fontWeight: 700, fontSize: '2rem', lineHeight: 1.2 },
-  h5: { fontFamily: '\'Plus Jakarta Sans\', sans-serif', fontWeight: 700, fontSize: '1.5rem', lineHeight: 1.2 },
-  h6: { fontFamily: '\'Plus Jakarta Sans\', sans-serif', fontWeight: 700, fontSize: '1.125rem', lineHeight: 1.2 },
+  h1: { fontWeight: 900, fontSize: '3.5rem', lineHeight: 1.2 },
+  h2: { fontWeight: 900, fontSize: '3rem', lineHeight: 1.2 },
+  h3: { fontWeight: 900, fontSize: '2.25rem', lineHeight: 1.2 },
+  h4: { fontWeight: 900, fontSize: '2rem', lineHeight: 1.2 },
+  h5: { fontWeight: 900, fontSize: '1.5rem', lineHeight: 1.2 },
+  h6: { fontWeight: 900, fontSize: '1.125rem', lineHeight: 1.2 },
 });
