dd7580fa8611280976502ca563c95e0b50d16913
Léo <leogaudinoff@gmail.com>
2023-05-20 22:48:51 +0200

    refactor(client): :coffin: Remove dead code

--- web/client/src/components/BoxesOverview.jsx
+++ web/client/src/components/BoxesOverview.jsx
@@ -1,9 +1,7 @@
-import { Alert, Table, Card, TableHead, TableCell, TableRow, TableBody, Typography, CardContent, Pagination, Stack } from '@mui/material';
 import { useState } from 'react';
 import BoxSummary from './BoxSummary';
 import { SeverityPill } from './SeverityPill';
 import { timeAgo } from '../service/timeAgo'
-import SkeletonTable from './SkeletonTable';
 import TableCard from './TableCard';
 
 const statusMap = {
@@ -41,106 +39,4 @@ export default function BoxesOverview({ boxes, scans, pageSize = 10 }) {
 			/>
 		</TableCard>
 	);
-
-	// const [page, setPage] = useState(1);
-
-	// const sortedBoxes = boxes
-	// ? boxes
-	// 	.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-	// 	.slice((page - 1) * pageSize, page * pageSize)
-	// : null;
-
-	// const handleChangePage = (event, newPage) => {
-	// 	setPage(newPage);
-	// };
-	// if (boxes !== null)
-	// return (
-	// 	<Card style={{ width: '100%', height: '100%', overflow: 'auto', alignItems: 'center' }}>
-	// 		<CardContent>
-	// 			<Typography variant="overline">Your tracked items</Typography>
-	// 			{(boxes.length !== 0) ?
-	// 				(
-	// 					<Stack direction='column' spacing={3} alignItems={'center'}>
-	// 						<Table>
-	// 							<TableHead>
-	// 								<TableRow>
-	// 									<TableCell>ID</TableCell>
-	// 									<TableCell>Recipient</TableCell>
-	// 									<TableCell>Created</TableCell>
-	// 									<TableCell>Status</TableCell>
-	// 								</TableRow>
-	// 							</TableHead>
-	// 							<TableBody>
-	// 								{sortedBoxes.map((box) => (
-	// 									<TableRow key={box?.id} onClick={() => {
-	// 										setBoxID(box?.id);
-	// 										setBoxDialogOpen(true);
-	// 									}} hover>
-	// 										<TableCell>
-	// 											<Typography fontSize={'.8rem'}>
-	// 												<code>{box?.id}</code>
-	// 											</Typography>
-	// 										</TableCell>
-	// 										<TableCell>{box?.school}</TableCell>
-	// 										<TableCell>
-	// 											<Typography fontSize={'.9rem'}>
-	// 												{timeAgo(box?.createdAt)}
-	// 											</Typography>
-	// 										</TableCell>
-	// 										<TableCell>
-	// 											<SeverityPill color={statusMap['inprogress']}>
-	// 												{'In Progress'}
-	// 											</SeverityPill>
-	// 										</TableCell>
-	// 									</TableRow>
-	// 								))}
-	// 							</TableBody>
-	// 						</Table>
-	// 						<Pagination
-	// 							count={Math.ceil(boxes.length / pageSize)}
-	// 							page={page}
-	// 							onChange={handleChangePage}
-	// 							color='primary'
-	// 							variant='outlined'
-	// 							defaultPage={0}
-	// 						/>
-	// 					</Stack>
-	// 				)
-	// 				:
-	// 				(boxes === null
-	// 					? <Typography variant='body1'>You have no boxes</Typography>
-	// 					: <SkeletonTable rows={10}/>
-	// 				)
-	// 			}
-	// 			<BoxSummary
-	// 				boxes={boxes}
-	// 				scans={scans}
-	// 				id={boxID}
-	// 				open={boxDialogOpen}
-	// 				setOpen={setBoxDialogOpen}
-	// 			/>
-	// 		</CardContent>
-	// 	</Card>
-	// );
-	// else
-	// 	return (
-	// 	<Card
-	// 		style={{
-	// 			width: '100%',
-	// 			height: '100%',
-	// 			overflow: 'auto'
-	// 		}}
-	// 	>
-	// 		<CardContent>
-	// 			<Typography
-	// 				variant="overline"
-	// 			>
-	// 				Boxes
-	// 			</Typography>
-	// 			<Alert severity='info'>
-	// 				You have no boxes yet.
-	// 			</Alert>
-	// 		</CardContent>
-	// 	</Card>
-	// 	);
 }
--- web/client/src/components/ScansOverview.jsx
+++ web/client/src/components/ScansOverview.jsx
@@ -1,9 +1,7 @@
-import { Alert, Table, Card, TableHead, TableCell, TableRow, TableBody, Typography, CardContent, Tooltip } from '@mui/material';
 import { timeAgo } from '../service/timeAgo';
 import { feature as getCountryName } from '@rapideditor/country-coder';
 import { useState } from 'react';
 import BoxSummary from './BoxSummary';
-import SkeletonTable from './SkeletonTable';
 import TableCard from './TableCard';
 
 export default function ScansOverview({ boxes, scans, disableDialogs = false }) {
@@ -38,118 +36,4 @@ export default function ScansOverview({ boxes, scans, disableDialogs = false }) 
 			/>}
 		</TableCard>
 	);
-
-	// if (scans !== null && boxes !== null)
-	// return (
-	// 	<Card
-	// 		style={{
-	// 			width: '100%',
-	// 			height: '100%',
-	// 			overflow: 'auto'
-	// 		}}>
-	// 		{!boxes.length || !scans.length
-	// 			? (
-	// 				<CardContent>
-	// 					<Typography
-	// 						variant="overline"
-	// 					>Scans</Typography>
-	// 					{
-	// 						<SkeletonTable rows={10} />
-	// 					}
-	// 				</CardContent>
-	// 			)
-	// 			: (
-	// 				<CardContent>
-	// 					<Typography
-	// 						variant="overline"
-	// 					>Scans</Typography>
-	// 					<Table>
-	// 						<TableHead>
-	// 							<TableRow>
-	// 								<TableCell>
-	// 									Box
-	// 								</TableCell>
-	// 								<TableCell>
-	// 									Operator
-	// 								</TableCell>
-	// 								<TableCell>
-	// 									Location
-	// 								</TableCell>
-	// 								<TableCell sortDirection={'asc'}>
-	// 									Time
-	// 								</TableCell>
-	// 								<TableCell>
-	// 									Comment
-	// 								</TableCell>
-	// 							</TableRow>
-	// 						</TableHead>
-	// 						<TableBody>
-	// 							{scanData.map(scan => {
-	// 								return (
-	// 									<TableRow
-	// 										id={scan?.id}
-	// 										key={scan?.id}
-	// 										onClick={() => {
-	// 											setBoxID(scan?.boxId);
-	// 											setBoxDialogOpen(true);
-	// 										}}
-	// 										hover
-	// 									>
-	// 										<TableCell>
-	// 											<Typography fontSize={'.8rem'}><code>{scan?.boxId}</code></Typography>
-	// 										</TableCell>
-	// 										<TableCell>
-	// 											<Typography fontSize={'.8rem'}><code>{scan?.operatorId}</code></Typography>
-	// 										</TableCell>
-	// 										<TableCell>
-	// 											{getCountryName([scan?.location.coords.longitude, scan?.location.coords.latitude], { level: 'territory' }).properties.nameEn}
-	// 										</TableCell>
-	// 										<TableCell>
-	// 											<Tooltip title={new Date(scan?.time).toLocaleString()}>
-	// 												<Typography fontWeight={'bold'} fontSize={'.9rem'}>
-	// 													{timeAgo(scan?.time)}
-	// 												</Typography>
-	// 											</Tooltip>
-	// 										</TableCell>
-	// 										<TableCell>
-	// 											<Typography fontSize={'smaller'} fontStyle={'italic'}>
-	// 												{scan?.comment}
-	// 											</Typography>
-	// 										</TableCell>
-	// 									</TableRow>
-	// 								)
-	// 							})}
-	// 						</TableBody>
-	// 					</Table>
-	// 					<BoxSummary
-	// 						boxes={boxes}
-	// 						scans={scanData}
-	// 						id={boxID}
-	// 						open={disableDialogs ? null : boxDialogOpen}
-	// 						setOpen={disableDialogs ? null : setBoxDialogOpen}
-	// 					/>
-	// 				</CardContent>
-	// 			)
-	// 		}
-	// 	</Card>
-	// );
-	// else
-	// 	return (
-	// 		<Card
-	// 			style={{
-	// 				width: '100%',
-	// 				height: '100%',
-	// 				overflow: 'auto'
-	// 			}}
-	// 		>
-	// 			<CardContent>
-	// 				<Typography
-	// 					variant="overline"
-	// 				>Scans</Typography>
-	// 				<Alert severity='info'>
-	// 					You have no scans on your boxes yet.
-	// 				</Alert>
-	// 			</CardContent>
-	// 		</Card>
-	// 	);
 }
