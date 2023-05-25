a938591641202273bff6aaf1894e9b2a743966e2
Léo <leogaudinoff@gmail.com>
2023-05-25 19:27:03 +0200

    build(global): :truck: Move sorting of entries to server-side

--- web/server/controllers/controller.js
+++ web/server/controllers/controller.js
@@ -210,7 +210,7 @@ const getAll = (Model, apiKeyNeeded = true) => async (req, res) => {
       });
     }
 
-    const instances = await Model.find({});
+    const instances = await Model.find({}).sort({ _id: -1 });
 
     if (!instances.length) {
       return handle404Error(res);
