d57448bfdac40abd8db5be6bfd9559d282148370
Léo <leogaudinoff@gmail.com>
2023-05-30 17:28:27 +0200

    fix(api): :bug: Try to fix DB acting nonsense

--- web/server/db/index.js
+++ web/server/db/index.js
@@ -4,8 +4,17 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 mongoose
-    .connect(process.env.STRING_URI, { useNewUrlParser: true })
-    .catch(e => {
+    .connect(process.env.STRING_URI,
+        {
+            useNewUrlParser: true,
+            server: {
+                socketOptions: {
+                    keepAlive: 100,
+                    connectTimeoutMS: 30000
+                }
+            }
+        }
+    ).catch(e => {
         console.error('Connection error', e.message)
     })
 
