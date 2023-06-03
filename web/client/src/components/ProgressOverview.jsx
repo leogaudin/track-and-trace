ac4b73b79763c4dab854803d6b1ab5d5a0e0b641
Léo <leogaudinoff@gmail.com>
2023-06-03 21:16:43 +0200

    feat(client): :lock: Add security

--- web/client/src/components/ProgressOverview.jsx
+++ web/client/src/components/ProgressOverview.jsx
@@ -6,36 +6,37 @@ import { Link } from 'react-router-dom';
 const ProgressOverview = ({ boxes, scans }) => {
   const deliveryResults = boxes && scans ? calculateDeliveryPercentage(boxes, scans) : [];
 
-  return (
-    <Card>
-      <CardContent>
-        <Typography variant="overline" gutterBottom>
-          Delivery Statuses
-        </Typography>
-        {boxes?.length && scans?.length ? (
-          <Grid container spacing={2}>
-            {deliveryResults.map((result) => (
-              <Grid item xs={9} sm={6} md={3} key={result.project}>
-                <Link to={`/boxes?query=${result.project}`} style={{ textDecoration: 'none' }}>
-                  <Card>
-                    <CardContent>
-                      <Typography variant="subtitle1">{result.project}</Typography>
-                      <Typography variant="h4" component="div" gutterBottom>
-                        {result.deliveryPercentage}%
-                      </Typography>
-                      <LinearProgress color={'success'} variant="determinate" value={result.deliveryPercentage} />
-                    </CardContent>
-                  </Card>
-                </Link>
-              </Grid>
-            ))}
-          </Grid>
-        ) : (
-          <Skeleton variant="text" />
-        )}
-      </CardContent>
-    </Card>
-  );
+  if (boxes?.length)
+    return (
+      <Card>
+        <CardContent>
+          <Typography variant="overline" gutterBottom>
+            Delivery Statuses
+          </Typography>
+          {boxes?.length && scans?.length ? (
+            <Grid container spacing={2}>
+              {deliveryResults.map((result) => (
+                <Grid item xs={9} sm={6} md={3} key={result.project}>
+                  <Link to={`/boxes?query=${result.project}`} style={{ textDecoration: 'none' }}>
+                    <Card>
+                      <CardContent sx={{'&:hover': {backgroundColor: 'neutral.100'}}}>
+                        <Typography variant="subtitle1">{result.project}</Typography>
+                        <Typography variant="h4" component="div" gutterBottom>
+                          {result.deliveryPercentage}%
+                        </Typography>
+                        <LinearProgress color={'success'} variant="determinate" value={result.deliveryPercentage} />
+                      </CardContent>
+                    </Card>
+                  </Link>
+                </Grid>
+              ))}
+            </Grid>
+          ) : (
+            <Skeleton variant="text" />
+          )}
+        </CardContent>
+      </Card>
+    );
 };
 
 export default ProgressOverview;
