fcf327eb9123cb22078a4daa6192bbad2248d866
Léo <leogaudinoff@gmail.com>
2023-06-03 17:14:33 +0200

    feat(server): :sparkles: Add deleteMany method 2

--- web/server/routes/boxes.router.js
+++ web/server/routes/boxes.router.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post('/box', BoxCtrl.createBox);
 router.post('/boxes', BoxCtrl.createBoxes);
 router.delete('/box/:id', BoxCtrl.deleteBox);
+router.delete('/boxes', BoxCtrl.deleteBoxes);
 router.get('/box/:id', BoxCtrl.getBoxById);
 router.get('/boxes', BoxCtrl.getBoxes);
 router.get('/boxes/:adminId', BoxCtrl.getBoxesByAdminId);
