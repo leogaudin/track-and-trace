93d41ae91f14106e401de48b5af6482951c9326e
Léo <leogaudinoff@gmail.com>
2023-07-09 16:56:55 +0200

    refactor: refactor code

--- web/server/controllers/controller/operations/createMany.js
+++ web/server/controllers/controller/operations/createMany.js
@@ -2,94 +2,69 @@ const lzstring = require('lz-string');
 const { handle400Error, handle201Success, handle206Success } = require('../errorHandlers');
 const { requireApiKey } = require('../apiKey');
 
+async function processInstances(instances, Model, apiKeyNeeded) {
+  const validInstances = [];
+  const invalidInstances = [];
+
+  let apiKeyChecked = false;
+  if (apiKeyNeeded) {
+    try {
+      await requireApiKey(req, res, async () => apiKeyChecked = true);
+    } catch (error) {
+      console.error('Error occurred during API key check:', error);
+      return handle400Error(res, error);
+    }
+  }
+
+  for (const instance of instances) {
+    if (!instance.id) {
+      invalidInstances.push({ instance, error: `You must provide an id for the item` });
+      continue;
+    }
+
+    if (apiKeyNeeded && !apiKeyChecked) {
+      invalidInstances.push({ instance, error: 'API key check failed' });
+      continue;
+    }
+
+    try {
+      const existent = await Model.findOne({ id: instance.id });
+
+      if (existent)
+        invalidInstances.push({ instance, error: `Item with ID ${existent.id} already exists` });
+      else
+        validInstances.push(instance);
+    } catch (error) {
+      console.error('Error occurred during existing item check:', error);
+      return handle400Error(res, error);
+    }
+  }
+
+  try {
+    await Model.insertMany(validInstances);
+
+    if (invalidInstances.length > 0)
+      return handle206Success(res, 'Some items were not created', invalidInstances, validInstances);
+    else
+      return handle201Success(res, 'Items created!', invalidInstances, validInstances);
+  } catch (error) {
+    console.error('Error occurred during database insertion:', error);
+    return handle400Error(res, error);
+  }
+}
+
 const createMany = (Model, apiKeyNeeded = true) => async (req, res) => {
-	try {
-		const { data } = req.body;
-		const payload = lzstring.decompressFromEncodedURIComponent(data);
-		const instances = JSON.parse(payload);
-		processInstances(instances);
-
-		async function processInstances(instances) {
-			const validInstances = [];
-			const invalidInstances = [];
-
-			// Check API key only once
-			let apiKeyChecked = false;
-			if (apiKeyNeeded) {
-				try {
-					await requireApiKey(req, res, async () => {
-						apiKeyChecked = true;
-					});
-				} catch (error) {
-					console.error('Error occurred during API key check:', error);
-					return handle400Error(res, error);
-				}
-			}
-
-			for (const instance of instances) {
-				if (!instance.id) {
-					invalidInstances.push({
-						instance,
-						error: `You must provide an id for the item`,
-					});
-					continue;
-				}
-
-				if (apiKeyNeeded && !apiKeyChecked) {
-					// Skip processing instances if API key check failed
-					invalidInstances.push({
-						instance,
-						error: 'API key check failed',
-					});
-					continue;
-				}
-
-				try {
-					const existent = await Model.findOne({ id: instance.id });
-
-					if (existent) {
-						invalidInstances.push({
-							instance,
-							error: `Item with ID ${existent.id} already exists`,
-						});
-					} else {
-						validInstances.push(instance);
-					}
-				} catch (error) {
-					console.error('Error occurred during existing item check:', error);
-					return handle400Error(res, error);
-				}
-			}
-
-			try {
-				await Model.insertMany(validInstances);
-
-				if (invalidInstances.length > 0) {
-					return handle206Success(
-						res,
-						'Some items were not created',
-						invalidInstances,
-						validInstances
-					);
-				} else {
-					return handle201Success(
-						res,
-						'Items created!',
-						invalidInstances,
-						validInstances
-					);
-				}
-			} catch (error) {
-				console.error('Error occurred during database insertion:', error);
-				return handle400Error(res, error);
-			}
-		}
-	} catch (error) {
-		console.error('Error occurred during createMany:', error);
-		return handle400Error(res, error);
-	}
+  try {
+    const { data } = req.body;
+    const payload = lzstring.decompressFromEncodedURIComponent(data);
+    const instances = JSON.parse(payload);
+    processInstances(instances, Model, apiKeyNeeded);
+  } catch (error) {
+    console.error('Error occurred during createMany:', error);
+    return handle400Error(res, error);
+  }
 };
 
 module.exports = {
-	createMany,
+  createMany,
 };
