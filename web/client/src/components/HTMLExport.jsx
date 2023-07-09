91922275ffdf5889079f03f61c4cf7e9274f5616
Léo <leogaudinoff@gmail.com>
2023-07-09 16:50:14 +0200

    refactor: refactor Exporter

--- web/client/src/components/HTMLExport.jsx
+++ web/client/src/components/HTMLExport.jsx
@@ -6,9 +6,8 @@ import { Button } from '@mui/material';
 
 const HTMLExport = ({ objects, folderName = 'Documents', itemName = 'Item' }) => {
   const generateHTML = (object) => {
-    const { school, project, division, district, zone, institutionType, htName, htPhone, createdAt, id } = object;
     const qrCode = new QRCode({
-      content: id,
+      content: object.id,
       width: 150,
       height: 150,
       padding: 0,
@@ -21,67 +20,41 @@ const HTMLExport = ({ objects, folderName = 'Documents', itemName = 'Item' }) =>
     qrCodeSvg = qrCodeSvg.replace(/<rect/g, '<rect rx="7"');
     qrCodeSvg = qrCodeSvg.replace(/ style="fill:#000000;shape-rendering:crispEdges;"/g, '');
 
-	const htmlStyles = `
-	  body {
-		font-family: sans-serif, Helvetica Neue, Helvetica, Arial;
-		margin: 0;
-		padding: 0;
-    font-weight: 100;
-	  }
-	  .document-container {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		height: 100vh;
-		padding: 20px;
-		box-sizing: border-box;
-		background-color: #f5f5f5;
-	  }
-	  .content-wrapper {
-		display: flex;
-		align-items: center;
-		max-width: 800px;
-		background-color: #ffffff;
-		box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
-		border-radius: 4px;
-		padding: 20px;
-	  }
-	  .info-container {
-		flex: 1;
-	  }
-	  .info-heading {
-		color: #424242;
-		font-size: 12px;
-		text-transform: uppercase;
-		margin-bottom: 10px;
-    font-weight: 100;
-	  }
-	  .info-row {
-		display: flex;
-		align-items: center;
-		margin-bottom: 10px;
-	  }
-	  .info-label {
-		font-weight: normal;
-		flex: 0 0 120px;
-	  }
-	  .info-value {
-		flex-grow: 1;
-	  }
-	  .qrcode-container {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		margin-left: 20px;
-	  }
-	  .qrcode-value {
-		font-family: 'Consolas', 'Monaco', 'Lucida Console', 'Liberation Mono', 'DejaVu Sans Mono', 'Bitstream Vera Sans Mono', 'Courier New';
-		font-size: 0.7rem;
-		margin-top: 5px;
-	  }
-	  `;
+    return htmlContent(object, qrCodeSvg);
+  };
 
-    const htmlContent = `
+  const downloadDocuments = () => {
+    const zip = new JSZip();
+    const promises = objects.map((object, index) => {
+      const htmlContent = generateHTML(object);
+      const fileName = `${itemName}-${object.id}.html`;
+      zip.file(fileName, htmlContent);
+    });
+
+    Promise.all(promises).then(() => {
+      zip.generateAsync({ type: 'blob', compression: 'DEFLATE', compressionOptions: { level: 9 } })
+        .then((content) => {
+          saveAs(content, `${folderName}.zip`);
+        })
+        .catch((error) => {
+          console.error('Failed to generate ZIP file:', error);
+        });
+    });
+  };
+
+  return (
+    <div>
+      <Button variant={'contained'} color='success' size='large' onClick={downloadDocuments}>
+        Download ZIP
+      </Button>
+    </div>
+  );
+};
+
+const htmlContent = (object, qrCodeSvg) => {
+  const { school, project, division, district, zone, institutionType, htName, htPhone, createdAt, id } = object;
+
+  return `
       <!DOCTYPE html>
       <html>
         <head>
@@ -140,36 +113,66 @@ const HTMLExport = ({ objects, folderName = 'Documents', itemName = 'Item' }) =>
         </body>
       </html>
     `;
-
-    return htmlContent;
-  };
-
-  const downloadDocuments = () => {
-    const zip = new JSZip();
-    const promises = objects.map((object, index) => {
-      const htmlContent = generateHTML(object);
-      const fileName = `${itemName}-${object.id}.html`;
-      zip.file(fileName, htmlContent);
-    });
-
-    Promise.all(promises).then(() => {
-      zip.generateAsync({ type: 'blob', compression: 'DEFLATE', compressionOptions: { level: 9 } })
-        .then((content) => {
-          saveAs(content, `${folderName}.zip`);
-        })
-        .catch((error) => {
-          console.error('Failed to generate ZIP file:', error);
-        });
-    });
-  };
-
-  return (
-    <div>
-      <Button variant={'contained'} color='success' size='large' onClick={downloadDocuments}>
-        Download ZIP
-      </Button>
-    </div>
-  );
 };
+
+const htmlStyles = `
+	  body {
+		font-family: sans-serif, Helvetica Neue, Helvetica, Arial;
+		margin: 0;
+		padding: 0;
+    font-weight: 100;
+	  }
+	  .document-container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		padding: 20px;
+		box-sizing: border-box;
+		background-color: #f5f5f5;
+	  }
+	  .content-wrapper {
+		display: flex;
+		align-items: center;
+		max-width: 800px;
+		background-color: #ffffff;
+		box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+		border-radius: 4px;
+		padding: 20px;
+	  }
+	  .info-container {
+		flex: 1;
+	  }
+	  .info-heading {
+		color: #424242;
+		font-size: 12px;
+		text-transform: uppercase;
+		margin-bottom: 10px;
+    font-weight: 100;
+	  }
+	  .info-row {
+		display: flex;
+		align-items: center;
+		margin-bottom: 10px;
+	  }
+	  .info-label {
+		font-weight: normal;
+		flex: 0 0 120px;
+	  }
+	  .info-value {
+		flex-grow: 1;
+	  }
+	  .qrcode-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-left: 20px;
+	  }
+	  .qrcode-value {
+		font-family: 'Consolas', 'Monaco', 'Lucida Console', 'Liberation Mono', 'DejaVu Sans Mono', 'Bitstream Vera Sans Mono', 'Courier New';
+		font-size: 0.7rem;
+		margin-top: 5px;
+	  }
+	`;
 
 export default HTMLExport;
