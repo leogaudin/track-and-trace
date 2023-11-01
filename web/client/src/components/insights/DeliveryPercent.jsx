155bedb5d3bfe0dabb82a0326f261eb39f00cc84
Léo <leogaudinoff@gmail.com>
2023-11-01 13:40:57 +0100

    feat: improve language support

--- web/client/src/components/insights/DeliveryPercent.jsx
+++ web/client/src/components/insights/DeliveryPercent.jsx
@@ -6,7 +6,7 @@ export default function DeliveryPercent({projectName, deliveryPercentage}) {
 		<>
 			<Typography variant="subtitle1">{projectName}</Typography>
 			<Typography variant="h4" component="div" gutterBottom>
-				{deliveryPercentage}% <span style={{fontWeight: 200, fontSize: 'smaller'}}>delivered</span>
+				{deliveryPercentage}% <span style={{fontWeight: 200, fontSize: 'smaller'}}>{t('delivered')}</span>
 			</Typography>
 			<LinearProgress color={'success'} variant="determinate" value={deliveryPercentage} />
 		</>
