2ad7d760eb36d2af57f50ddcebbb279d849fb928
Léo <leogaudinoff@gmail.com>
2024-01-24 14:33:52 +0200

    fix: fix security

--- web/server/controllers/boxes.ctrl.js
+++ web/server/controllers/boxes.ctrl.js
@@ -10,21 +10,21 @@ const deleteBox = deleteOne(Box);
 const deleteBoxes = deleteMany(Box);
 const getBoxesByAdminId = async (req, res) => {
     try {
-        const admin = await Admin.find({id: req.params.adminId});
-        if (!admin.publicInsights && !req.headers['x-authorization'])
-            return res.status(401).json({ success: false, error: 'API key required' });
-        // if (!req.headers['x-authorization']) {
-        //     return res.status(401).json({ success: false, error: 'API key required' });
-        // }
-        // const apiKey = req.headers['x-authorization'];
-        // const admin = await Admin.findOne({ apiKey });
+        const admin = await Admin.findOne({id: req.params.adminId});
+        if (!admin.publicInsights) {
+            const apiKey = req.headers['x-authorization'];
+            if (!apiKey)
+                return res.status(401).json({ success: false, error: 'API key required' });
+            else if (apiKey !== admin.apiKey) {
+                return res.status(401).json({ success: false, error: 'Invalid API key' });
+            }
+        }
 
-        // if (!admin) {
-        //     return res.status(401).json({ success: false, error: 'Invalid API key' });
-        // }
         const boxes = await Box.find({ adminId: req.params.adminId }).skip(parseInt(req.query.skip)).limit(parseInt(req.query.limit));
+
         if (!boxes.length)
             return res.status(404).json({ success: false, error: `No boxes available` });
+
         return res.status(200).json({ success: true, data: boxes });
     } catch (error) {
         console.log(error);
